8fa840b8c743e3867658345046f8459d61f6e237
Trilleo <gongsanle@gmail.com>
2026-04-22 19:15:23 +0800

    Improvement: Add start button to main UI

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/MainUI.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/MainUI.kt
@@ -5,9 +5,13 @@ import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.trilleo.mc.plugins.trihunt.enums.FillMode
 import net.trilleo.mc.plugins.trihunt.registration.PluginGUI
+import net.trilleo.mc.plugins.trihunt.utils.itemStack
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemFlag
 
 class MainUI : PluginGUI(
     id = "main",
@@ -16,6 +20,14 @@ class MainUI : PluginGUI(
     fillMode = FillMode.LIGHT
 ) {
     override fun setup(player: Player, inventory: Inventory) {
+        val startButton = itemStack(Material.GREEN_CONCRETE) {
+            name("<bold><gradient:green:dark_green>Start</gradient></bold>")
+            lore(" ", "<gray>Start the Manhunt")
+            enchant(Enchantment.KNOCKBACK, 1)
+            flag(ItemFlag.HIDE_ENCHANTS)
+        }
+
+        inventory.setItem(22, startButton)
     }
 
     override fun onClick(event: InventoryClickEvent) {
