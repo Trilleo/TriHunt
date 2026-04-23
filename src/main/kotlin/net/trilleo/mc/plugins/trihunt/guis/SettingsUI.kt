4c87185f0aeee2ce7dde2d460debec43b93d2f56
Trilleo <gongsanle@gmail.com>
2026-04-23 22:52:49 +0800

    Feature: Add SettingsUI.kt

---
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/guis/SettingsUI.kt
@@ -0,0 +1,55 @@
+package net.trilleo.mc.plugins.trihunt.guis
+
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.trilleo.mc.plugins.trihunt.enums.FillMode
+import net.trilleo.mc.plugins.trihunt.registration.GUIManager
+import net.trilleo.mc.plugins.trihunt.registration.PluginGUI
+import net.trilleo.mc.plugins.trihunt.utils.itemStack
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+
+class SettingsUI : PluginGUI(
+    id = "settings",
+    title = Component.text("TriHunt Settings").color(NamedTextColor.DARK_GRAY).decorate(TextDecoration.BOLD),
+    rows = 5,
+    fillMode = FillMode.LIGHT
+) {
+    val slotIndex: Map<String, Int> = mapOf(
+        "backButtonSlot" to 36,
+        "closeButtonSlot" to 40
+    )
+
+    override fun setup(player: org.bukkit.entity.Player, inventory: org.bukkit.inventory.Inventory) {
+        val closeButton = itemStack(Material.BARRIER) {
+            name("<bold><red>Close")
+        }
+        val backButton = itemStack(org.bukkit.Material.ARROW) {
+            name("<bold><gray>Back")
+        }
+
+        inventory.setItem(slotIndex.getValue("backButtonSlot"), backButton)
+        inventory.setItem(slotIndex.getValue("closeButtonSlot"), closeButton)
+    }
+
+    override fun onClick(event: InventoryClickEvent) {
+        event.isCancelled = true
+        val player = event.whoClicked as Player
+        if (event.slot in slotIndex.values) {
+            player.playSound(
+                Sound.sound(Key.key("minecraft:ui.button.click"), Sound.Source.UI, 1f, 1f)
+            )
+        }
+
+        if (event.slot == slotIndex.getValue("closeButtonSlot")) {
+            player.closeInventory()
+        }
+        if (event.slot == slotIndex.getValue("backButtonSlot")) {
+            GUIManager.open(player, "main")
+        }
+    }
+}
