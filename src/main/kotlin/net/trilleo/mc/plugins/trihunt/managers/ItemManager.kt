527dffd10ea4ebb7a170d828a6ce799cdf0c4e5a
Trilleo <gongsanle@gmail.com>
2026-04-28 13:54:56 +0800

    Feature: Add clear plugin items function to ItemManager.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/ItemManager.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/ItemManager.kt
@@ -1,9 +1,11 @@
 package net.trilleo.mc.plugins.trihunt.managers
 
 import net.trilleo.mc.plugins.trihunt.utils.PDCEntryUtil
+import net.trilleo.mc.plugins.trihunt.utils.PDCUtil
 import net.trilleo.mc.plugins.trihunt.utils.itemStack
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
@@ -40,5 +42,21 @@ class ItemManager(private val plugin: JavaPlugin) {
             )
         }
         return compassItem
+    }
+
+    fun clearPluginItems(player: Player) {
+        for (item in player.inventory.contents) {
+            if (item != null && PDCUtil.get(
+                    item,
+                    PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                    PersistentDataType.STRING
+                ) in listOf(
+                    PDCEntryUtil.PDCValue().mainItemIdentifier,
+                    PDCEntryUtil.PDCValue().compassItemIdentifier
+                )
+            ) {
+                player.inventory.remove(item)
+            }
+        }
     }
 }
