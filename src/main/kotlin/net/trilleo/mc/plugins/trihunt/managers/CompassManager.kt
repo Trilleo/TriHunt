2aa04a0bdb25f49a071686ab52a404a12160ada6
Trilleo <gongsanle@gmail.com>
2026-04-28 15:28:13 +0800

    Feature: Add compass tracking logic to CompassManager.kt

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/CompassManager.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/managers/CompassManager.kt
@@ -1,4 +1,39 @@
 package net.trilleo.mc.plugins.trihunt.managers
 
-object CompassManager {
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.trilleo.mc.plugins.trihunt.utils.TeamUtil
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+
+class CompassManager(private val plugin: JavaPlugin) {
+    fun refreshCompass(player: Player, compassItem: ItemStack) {
+        var speedrunnerUUIDs = TeamUtil.getTeam("speedrunner")?.members
+        if (speedrunnerUUIDs != null) {
+            for (uuid in speedrunnerUUIDs) {
+                val target = Bukkit.getPlayer(uuid)
+
+                if (target != null && target.isOnline) {
+
+                    if (player.world == target.world) {
+                        player.compassTarget = target.location
+                        player.sendActionBar(
+                            Component.text("Tracked player ").color(NamedTextColor.GREEN)
+                                .append(Component.text(target.name).color(NamedTextColor.YELLOW))
+                        )
+                    } else {
+                        player.sendActionBar(
+                            Component.text("Player ").color(NamedTextColor.RED)
+                                .append(Component.text(target.name).color(NamedTextColor.YELLOW))
+                                .append(Component.text(" is in a different world!").color(NamedTextColor.RED))
+                        )
+                    }
+
+                    break
+                }
+            }
+        }
+    }
 }
