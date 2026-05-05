b337f66e285d5e93a24125c4f307a637a2ee0f83
Trilleo <gongsanle@gmail.com>
2026-05-05 12:50:03 +0800

    Improvement: Reformat source code

--- src/main/kotlin/net/trilleo/mc/plugins/trihunt/recipes/BridgeEggRecipe.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trihunt/recipes/BridgeEggRecipe.kt
@@ -3,7 +3,6 @@ package net.trilleo.mc.plugins.trihunt.recipes
 import net.trilleo.mc.plugins.trihunt.items.BridgeEggItem
 import net.trilleo.mc.plugins.trihunt.registration.PluginRecipe
 import org.bukkit.Material
-import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.Recipe
 import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.plugin.java.JavaPlugin
